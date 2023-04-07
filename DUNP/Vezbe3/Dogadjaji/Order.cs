5484462d3e737b803c08795ab0a51e9c8b0396ce
bimmerboyy <tarikkucevic@gmail.com>
2023-04-07 14:09:29 +0200

    events

--- DUNP/Vezbe3/Dogadjaji/Order.cs
+++ DUNP/Vezbe3/Dogadjaji/Order.cs
@@ -1,66 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace CSharp.DUNP.Vezbe3.Dogadjaji;
 
-namespace CSharp.DUNP.Vezbe3.Dogadjaji
+public class CustomEventArgs : EventArgs
 {
-    public class CustomEventArgs : EventArgs
+    public Product Product { get; set; }
+}
+public class Order
+{
+    public delegate void OrderProcessEventHandler(object sender, CustomEventArgs e);
+    public event OrderProcessEventHandler OrderProcess;
+    public void Process(Product product)
     {
-        public Product Product { get; set; }
+        Console.WriteLine($"Processing product with id {product.Id}");
+        Thread.Sleep(3000);
+
+        OnOrederProcess(product);
+
+
     }
-    public class Order
+    public virtual void OnOrederProcess(Product product)
     {
-        public delegate void OrderProcessEventHandler(object sender,CustomEventArgs e);
-        public event OrderProcessEventHandler OrderProcess;
-       public void Process(Product product)
+        if (OrderProcess != null)
         {
-            Console.WriteLine($"Processing product with id {product.Id}");
-            Thread.Sleep(3000);
-
-            OnOrederProcess(product);
-
-            
+            OrderProcess(this, new CustomEventArgs { Product = product});
         }
-        public virtual void OnOrederProcess(Product product)
-        {
-            if(OrderProcess != null )
-            {
-                OrderProcess(this, EventArgs.Empty);
-            }
-           
 
-        }
+
     }
 }
 
 /*
- * Main funkcija
- * 
- * public class SMSService(){
+* Main funkcija
+* 
+* public class SMSService(){
 
-        public void OrderProcessed(object sender, CustomEventArgs e)
-    {
-        Console.WriteLine($"Hey I got request from order that order {e.Product.Name} has been processed  (SMS)");
-    }
-        }
+public void OrderProcessed(object sender, CustomEventArgs e)
+{
+Console.WriteLine($"Hey I got request from order that order {e.Product.Name} has been processed  (SMS)");
+}
+}
 public class EmailService(){
 
-    public void OrderProcessed(object sender, CustomEventArgs e)
-    {
-        Console.WriteLine($"Hey I got request from order that order {e.Product.Id} has been processed (Email) ");
-    }
+public void OrderProcessed(object sender, CustomEventArgs e)
+{
+Console.WriteLine($"Hey I got request from order that order {e.Product.Id} has been processed (Email) ");
+}
 }
 
- Order o = new Order();
-        SMSService sMS = new SMSService();
-        EmailService email = new EmailService();
-        o.OrderProcess += sMS.OrderProdessed;
-        o.OrderProcess += email.OrderProdessed;
+Order o = new Order();
+SMSService sMS = new SMSService();
+EmailService email = new EmailService();
+o.OrderProcess += sMS.OrderProdessed;
+o.OrderProcess += email.OrderProdessed;
 
-        o.Process();
+o.Process();
 
-            //Kreirati extention metodu koja skracuje naslov na doredjeni broj reci a u nastavku dodaje 
-            // ...
- */
+    //Kreirati extention metodu koja skracuje naslov na doredjeni broj reci a u nastavku dodaje 
+    // ...
+*/
